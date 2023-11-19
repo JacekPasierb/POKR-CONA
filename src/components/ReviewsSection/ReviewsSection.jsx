acf3dc00ef7f58f88e0822627d0f1618f27d36ed
Jacek Pasierb <jack_byk@o2.pl>
2023-11-19 23:48:13 +0100

    Button done

--- src/components/ReviewsSection/ReviewsSection.jsx
+++ src/components/ReviewsSection/ReviewsSection.jsx
@@ -2,43 +2,75 @@ import React from "react";
 import TitleSection from "../TitleSection/TitleSection";
 import css from "./ReviewsSection.module.css";
 import sprite from "../../images/icons/sprite.svg";
-
+import reviews from "../../../data/reviews";
 const ReviewsSection = () => {
   return (
-    <div>
+    <div className={css.reviewSection}>
       <TitleSection title1="Opinie Klientów" title2="Tak o nas piszą" />
-      <div className={css.reviewBox}>
-        <div className={css.line}></div>
-        <div>
-          <svg className={css.icon}>
-            <use href={sprite + `#icon-Heart`}></use>
-          </svg>
-          <svg className={css.icon}>
-            <use href={sprite + `#icon-Heart`}></use>
-          </svg>
-          <svg className={css.icon}>
-            <use href={sprite + `#icon-Heart`}></use>
-          </svg>
-          <svg className={css.icon}>
-            <use href={sprite + `#icon-Heart`}></use>
-          </svg>
-          <svg className={css.icon}>
-            <use href={sprite + `#icon-Heart`}></use>
-          </svg>
-        </div>
-        <p className={css.reviewOpinion}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa
-          mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla,
-          mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis
-          tellus.
-        </p>
-        <div className={css.reviewTitle}>
-          <p className={css.reviewAuthor}>Marlena Ł.</p>
-          <p className={css.reviewData}>8.11.2023</p>
-        </div>
-      </div>
+      <ul className={css.reviewsBox}>
+        {reviews.map((review) => (
+          <li key={review.id} className={css.reviewBox}>
+            <div className={css.line}></div>
+            <div>
+              <svg className={css.icon}>
+                <use href={sprite + `#icon-Heart`}></use>
+              </svg>
+              <svg className={css.icon}>
+                <use href={sprite + `#icon-Heart`}></use>
+              </svg>
+              <svg className={css.icon}>
+                <use href={sprite + `#icon-Heart`}></use>
+              </svg>
+              <svg className={css.icon}>
+                <use href={sprite + `#icon-Heart`}></use>
+              </svg>
+              <svg className={css.icon}>
+                <use href={sprite + `#icon-Heart`}></use>
+              </svg>
+            </div>
+            <p className={css.reviewOpinion}>{review.opinion}</p>
+            <div className={css.reviewTitle}>
+              <p className={css.reviewAuthor}>{review.author}</p>
+              <p className={css.reviewData}>{review.data}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+      <button type="button" className={css.button}>
+        Poczytaj na Booksach
+      </button>
     </div>
   );
 };
 
 export default ReviewsSection;
+
+//  <div className={css.reviewBox}>
+//    <div className={css.line}></div>
+//    <div>
+//      <svg className={css.icon}>
+//        <use href={sprite + `#icon-Heart`}></use>
+//      </svg>
+//      <svg className={css.icon}>
+//        <use href={sprite + `#icon-Heart`}></use>
+//      </svg>
+//      <svg className={css.icon}>
+//        <use href={sprite + `#icon-Heart`}></use>
+//      </svg>
+//      <svg className={css.icon}>
+//        <use href={sprite + `#icon-Heart`}></use>
+//      </svg>
+//      <svg className={css.icon}>
+//        <use href={sprite + `#icon-Heart`}></use>
+//      </svg>
+//    </div>
+//    <p className={css.reviewOpinion}>
+//      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi.
+//      Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis
+//      ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus.
+//    </p>
+//    <div className={css.reviewTitle}>
+//      <p className={css.reviewAuthor}>Marlena Ł.</p>
+//      <p className={css.reviewData}>8.11.2023</p>
+//    </div>
+//  </div>;
