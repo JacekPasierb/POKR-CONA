24bf365d1e97b6b6b0fe8ae24839bf6fd75982f3
Jacek Pasierb <jack_byk@o2.pl>
2023-12-01 11:58:38 +0100

    PortfolioPage styled Gallery

--- src/components/GalleryNais/GalleryNais.jsx
+++ src/components/GalleryNais/GalleryNais.jsx
@@ -3,20 +3,35 @@ import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
 import { useMediaQuery } from "react-responsive";
 import imagesPaznokcie from "../../../data/images.json";
+import css from "./GalleryNais.module.css";
 
 const GalleryNais = () => {
   const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
   const isTablet = useMediaQuery({
-    query: "(min-width: 768px) and (max-width: 1200px)",
+    query: "(min-width: 768px) ",
   });
 
-  const imagesMobile = [{}];
-  const imagesDesctop = [{}];
-
+  const imagesFix = imagesPaznokcie.map((image) => ({
+    original: isTablet
+      ? image.original.replace(".webp", "Tablet.webp")
+      : image.original,
+    thumbnail: isTablet
+      ? image.thumbnail.replace(".webp", "Tablet.webp")
+      : image.thumbnail,
+    originalClass: isTablet ? css.imageWidthTablet : css.imageWidth,
+  }));
+  const gallerySettings = {
+    showThumbnails: true,
+    showFullscreenButton: true,
+    showPlayButton: true,
+    items: imagesFix,
+    thumbnailPosition: "right",
+    slideInterval: 2000,
+  };
   return (
     <>
       <p>Paznokcie Sary</p>
-      <ImageGallery items={isTablet ? imagesDesctop : imagesMobile} />
+      <ImageGallery items={imagesFix} {...gallerySettings} />
     </>
   );
 };
